bff832070581cfbe35f7318378c1de9cc7b3d460
Gustavo Arantes <gustavoarantes@tuta.io>
2024-04-14 20:05:43 -0300

    Added logic to check if exists the class and method

--- app/index.php
+++ app/index.php
@@ -7,3 +7,18 @@ spl_autoload_register(function($class)
         require_once $class . '.php';
     }
 });
+
+$class = isset($_REQUEST['class']) ? $_REQUEST['class'] : $_REQUEST['class'] = 'OrderList';
+$method = isset($_REQUEST['method']) ? $_REQUEST['method'] : null;
+
+if (!empty($class) && class_exists($class))
+{
+    $page = new $class($_GET);
+
+    if (!empty($method) && method_exists($class, $method))
+    {
+        $page->$method($_GET);
+    }
+
+    $page->show();
+}
