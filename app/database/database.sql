9552c5d00f7276687c8c2c419effee18832feadd
Gustavo Arantes <gustavoarantes@tuta.io>
2024-04-14 14:57:54 -0300

    Created the files to configure the database

---
+++ app/database/database.sql
@@ -0,0 +1,16 @@
+createdb candydb
+
+psql -d candydb
+
+CREATE TABLE "order" 
+(
+    id SERIAL PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL, 
+    client VARCHAR(255) NOT NULL, 
+    endDate TIMESTAMP NOT NULL, 
+    price NUMERIC(10,2) NOT NULL, 
+    paymentMethod VARCHAR(100) NOT NULL, 
+    description TEXT, 
+    finished BOOLEAN DEFAULT FALSE, 
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
