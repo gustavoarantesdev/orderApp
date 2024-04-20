9096e9453f5ea76b1fd9c2f0eefac1358bd24827
Gustavo Arantes <gustavoarantes@tuta.io>
2024-04-19 21:57:37 -0300

    Configured basic functions in OrderFormUpdate.php

--- app/class/OrderFormUpdate.php
+++ app/class/OrderFormUpdate.php
@@ -4,5 +4,32 @@ require_once 'Order.php';
 
 class OrderFormUpdate
 {
+    private $html;
+    private $data;
 
+    public function __construct()
+    {
+        $file = dirname(__DIR__) . '/interfaces/form.html';
+        $this->html = file_get_contents($file);
+    }
+
+    public function save($param)
+    {
+        try
+        {
+            Order::save($param);
+            $this->data = $param;
+            print "Cadastrado com Sucesso";
+        }
+        catch (Exception $e)
+        {
+            print $e->getMessage();
+        }
+   }
+
+    public function show()
+    {
+        print $this->html;
+    }
+}
 }
