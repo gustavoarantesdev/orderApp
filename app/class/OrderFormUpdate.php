b5029110fb9db6c82e0b26df02ca2b5e2ab7674d
Gustavo Arantes <gustavoarantes@tuta.io>
2024-04-20 17:00:25 -0300

    Added find function, and replace logic in html

--- app/class/OrderFormUpdate.php
+++ app/class/OrderFormUpdate.php
@@ -9,8 +9,21 @@ class OrderFormUpdate
 
     public function __construct()
     {
-        $file = dirname(__DIR__) . '/interfaces/form.html';
+        $file = dirname(__DIR__) . '/interfaces/updateForm.html';
         $this->html = file_get_contents($file);
+    }
+
+    public function update($param)
+    {
+        try
+        {
+            $id = (int) $param['id'];
+            $this->data = Order::find($id);
+        }
+        catch (Exception $e)
+        {
+            print $e->getMessage();
+        }
     }
 
     public function save($param)
@@ -29,7 +42,16 @@ class OrderFormUpdate
 
     public function show()
     {
+        $this->html = str_replace('{id}', (string) $this->data['id'], $this->html);
+        $this->html = str_replace('{title}', (string) $this->data['title'], $this->html);
+        $this->html = str_replace('{client}', (string) $this->data['client'], $this->html);
+        $this->html = str_replace('{endDate}', (string) $this->data['enddate'], $this->html);
+        $this->html = str_replace('{price}', (string) $this->data['price'], $this->html);
+        $this->html = str_replace('{paymentMethod}', (string) $this->data['paymentmethod'], $this->html);
+        $this->html = str_replace('{description}', (string) $this->data['description'], $this->html);
+        $this->html = str_replace('{creationDate}', (string) $this->data['creationdate'], $this->html);
+
         print $this->html;
     }
 }
-}
+
