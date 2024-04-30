83054a5dc84943944e59e910e80066d322e1d08b
Gustavo Arantes <gustavoarantes@tuta.io>
2024-04-30 20:44:48 -0300

    Changed dateFormat method by adding withTime for boolean values

--- app/class/Utility.php
+++ app/class/Utility.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * This class is to centralize the auxiliary codes used in many parts of the code.
+ * @author gustavoarantes
+ */
 class Utility
 {
-    public static function dateFormat($date, $mode = 'null')
+    /**
+     * This method is used to convert dates to the Brazilian format dd/mm/yyyy h:s.
+     * @param $date - Date is the database date..
+     * @param $withTime - Receive boolean values, true when there is a timetable and false when the timetable is not needed.
+     * @return - returns the formatted date. 
+     */
+    public static function dateFormat($date, $withTime)
     {
-         return ($mode == 'noTime') ? date('d/m/y', strtotime($date)) :  date('d/m/y H:i', strtotime($date));
+        return ($withTime) ? date('d/m/y', strtotime($date)) : date('d/m/y H:i', strtotime($date));
     }
 
     public static function priceFormat($price)
