e7efa343dd3b55044dabc34df68c4be7ec0e58c4
Gustavo Arantes <gustavoarantes@tuta.io>
2024-09-17 12:46:06 -0300

    feat: Adicionado novos métodos

--- App/Controllers/OrderController.php
+++ App/Controllers/OrderController.php
@@ -4,8 +4,65 @@ namespace App\Controllers;
 
 class OrderController
 {
-    public function index(): void
+    /**
+     * Página inicial com todos os pedidos.
+     */
+    public function index()
     {
-        print 'OrderController->index()';
+        echo '<h1>Todos os pedidos disponiveis</h1>';
+    }
+
+    /**
+     * Exibe o formulário para cadastro de novo pedido.
+     */
+    public function create()
+    {
+        echo '<h1>Cadastrar novo pedidos</h1>';
+        //
+    }
+
+    /**
+     * Armazena um novo pedido.
+     */
+    public function store(string $id)
+    {
+        echo '<h1>Todos os pedidos</h1>';
+        //
+    }
+
+    /**
+     * Exibe todos os pedidos.
+     */
+    public function show()
+    {
+        echo '<h1>Todos os pedidos</h1>';
+        //
+    }
+
+    /**
+     * Exibe um formulário para edição de um pedido.
+     */
+    public function edit(string $id)
+    {
+        echo "<h1>Editar um pedidos {$id}</h1>";
+        //
+    }
+
+    /**
+     * Atualiza um pedido no banco de dados.
+     */
+    public function update(string $id)
+    {
+        echo '<h1>Todos os pedidos</h1>';
+        //
+    }
+
+    /**
+     * Delete um pedido no banco de dados.
+     */
+    public function delete(string $id)
+    {
+        echo '<h1>Deleta um pedidos</h1>';
+        //
     }
 }
