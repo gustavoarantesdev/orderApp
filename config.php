dcc75c27f674af936c3547e407f91655b87c5e55
Gustavo Arantes <gustavoarantes@tuta.io>
2024-09-17 21:56:50 -0300

    raw: Adicionado as credenciais

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
 
 define('BASE_URL', '/orderApp');
+
+// Credenciais do banco de dados
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'candydb');
+define('DB_USER', 'gustavoarantes');
+define('DB_PASS', '');
