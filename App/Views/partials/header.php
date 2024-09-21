48b4d1f1083b25702791d9c4392e400447f3eb1d
Gustavo Arantes <gustavoarantes@tuta.io>
2024-09-20 23:22:54 -0300

    fetch: Adicionado o link para lista todas encomendas.

--- App/Views/partials/header.php
+++ App/Views/partials/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $title ?? 'OrderApp' ?></title>
+    <title>OrderApp</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
@@ -22,6 +22,7 @@
         <nav class="navbar bg-body-tertiary">
             <div class="container">
                 <a class="navbar-brand" href="<?= BASE_URL ?>">OrderApp</a>
+                <a href="<?= BASE_URL . '/show' ?>" class="nav-link link-body-emphasis">Ver Todas</a>
             </div>
         </nav>
     </header>
