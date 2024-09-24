a3d9d67038b716ed1f01f652ed1e2000e7a4f772
Gustavo Arantes <gustavoarantes@tuta.io>
2024-09-23 21:49:46 -0300

    refactor: Ajustado o design pattern Singleton e adicionado o try...catch.

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -3,36 +3,66 @@
 namespace App\Core;
 
 use PDO;
+use PDOException;
 
+/**
+ * Classe responsável por centralizar a conexão com o banco de dados.
+ */
 abstract class Model
 {
     /**
-     * @var PDO|null The database connection object.
+     * @var string Credênciais para conexão com o banco de dados.
      */
     private string $db_host = DB_HOST;
     private string $db_user = DB_USER;
     private string $db_pass = DB_PASS;
     private string $db_name = DB_NAME;
 
+    /**
+     * @var PDO|null Conexão com o banco de dados (instância única).
+     */
     private static ?PDO $conn = null;
 
     /**
-     * Retrieves the database connection.
+     * Construtor privado para evitar instanciamento.
+     */
+    private function __construct()
+    {
+
+    }
+
+    /**
+     * Recupera a conexão com o banco de dados.
      *
-     * If the connection hasn't been established yet, it reads the database configuration from a file
-     * and creates a new PDO connection.
+     * Se a conexão ainda não foi estabelecida, ela é criada.
      *
-     * @return PDO The database connection object.
+     * @return PDO A conexão com o banco de dados.
      */
     protected function getConnection(): PDO
     {
-        if (is_null(self::$conn)) {
+        // Se a conexão já foi estabelecida, retorna ela mesma.
+        if (!is_null(self::$conn)) {
+            return self::$conn;
+        }
+
+        // Tenta criar uma conexão se ainda não existir.
+        try {
+            // Define o Data Source Name (DSN) para conexão.
             $dsn = "pgsql:host=$this->db_host;dbname=$this->db_name";
+
+            // Cria a nova conexão PDO.
             self::$conn = new PDO($dsn, $this->db_user, $this->db_pass);
 
+            // Define os atributos da conexão.
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        } catch (PDOException) {
+            http_response_code(500);
+            require __DIR__ . '/../Views/errors/500.php';
+            exit;
         }
+
+        // Retorna a conexão recém-estabelecida.
         return self::$conn;
     }
 }
