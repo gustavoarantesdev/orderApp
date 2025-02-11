3c5e2f736b92d8b4ffb33470fd38667f340d406f
Gustavo Arantes <gustavoarantes@gmail.com>
2025-02-10 22:15:20 -0300

    feat: Adicionar mascara para o input de quantidade

--- public/js/inputMask.js
+++ public/js/inputMask.js
@@ -4,4 +4,5 @@ $(document).ready(function () {
     $('.cpf').mask('000.000.000-00');
     $('.cnpj').mask('00.000.000/0000-00');
     $('.money').mask('00.000,00', { reverse: true });
+    $('.quantity').mask('0000', { reverse: true });
 });
