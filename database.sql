e5e7ff6435ec6d5c8d82de502c21deb1c0ea214c
Gustavo Arantes <gustavoarantes@gmail.com>
2025-02-10 21:31:45 -0300

    feat: Adicionar tabelas para Encomenda e Itens da encomenda

--- database.sql
+++ database.sql
@@ -43,21 +43,42 @@ CREATE TABLE products (
     created_at  TIMESTAMP      DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Tabela de pedidos
 CREATE TABLE orders (
-    order_id                   SERIAL        PRIMARY KEY,
-    user_id                    INTEGER       NOT NULL,
-    order_title                VARCHAR(150)  NOT NULL,
-    order_quantity             SMALLINT      NOT NULL,
-    order_client_name          VARCHAR(150)  NOT NULL,
-    order_withdraw             BOOLEAN       DEFAULT FALSE,
-    order_completion_date      DATE          NOT NULL,
-    order_completion_time      TIME          NOT NULL,
-    order_delivery_address     VARCHAR(150)  NOT NULL,
-    order_price                NUMERIC(10,2) NOT NULL,
-    order_payment_method       SMALLINT      NOT NULL,
-    order_payment_installments SMALLINT      NOT NULL,
-    order_description          TEXT,
-    order_completed            BOOLEAN       DEFAULT FALSE,
-    order_created_at           TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    id                   SERIAL        PRIMARY KEY,
+    user_id              INTEGER       NOT NULL,
+    customer_id          INTEGER       NOT NULL,
+    order_number         INTEGER       NOT NULL,
+    delivery_address     VARCHAR(150)  NOT NULL,
+    additional           NUMERIC(10,2) NOT NULL,
+    discount             NUMERIC(10,2) NOT NULL,
+    subtotal             NUMERIC(10,2) NOT NULL,
+    payment_value        NUMERIC(10,2) NOT NULL,
+    payment_status       SMALLINT      NOT NULL,
+    payment_method       SMALLINT      NOT NULL,
+    payment_installments SMALLINT      NOT NULL,
+    payment_date         DATE          NOT NULL,
+    completion_date      DATE          NOT NULL,
+    completion_time      TIME          NOT NULL,
+    withdraw             BOOLEAN       DEFAULT FALSE,
+    order_status         SMALLINT      NOT NULL,
+    description          VARCHAR(255)  DEFAULT NULL,
+    created_at           TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id)        REFERENCES users(id),
+    FOREIGN KEY (customer_id)    REFERENCES customers(id)
+);
+
+-- Tabela de itens do pedido.
+CREATE TABLE order_items (
+    id          SERIAL         PRIMARY KEY,
+    user_id     INTEGER        NOT NULL,
+    order_id    INTEGER        NOT NULL,
+    product_id  INTEGER        NOT NULL,
+    sell_price  NUMERIC(10, 2) NOT NULL,
+    quantity    INTEGER        NOT NULL,
+
+    FOREIGN KEY (user_id)    REFERENCES users(id),
+    FOREIGN KEY (order_id)   REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
