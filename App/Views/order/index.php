75566413bba38875e46730ced4d2e0a1bb8694e3
Gustavo Arantes <gustavoarantes@gmail.com>
2025-02-18 22:21:50 -0300

    fix: Ajustar as views para apresentar as encomendas registradas

--- App/Views/order/index.php
+++ App/Views/order/index.php
@@ -10,12 +10,18 @@ FlashMessage::render();
 <main style="margin-bottom: 7rem;">
     <section class="container">
         <h1 class="my-4 text-center" style="font-size: 1.8rem;">Suas Encomendas</h1>
-        <div class="row row-cols-sm-2 row-cols-md-3 g-3 d-flex justify-content-center">
-            <?php foreach ($ordersData as $orderData) { ?>
+        <div class="row row-cols-sm-2 row-cols-md-3 g-3 d-flex">
+            <?php foreach ($viewData as $orderData) { ?>
                 <!-- Encomenda -->
                 <div class="col mt-5" style="min-width: 22rem;">
                     <div class="bg-body position-relative rounded-5 p-3 border shadow">
-                        <p hidden><?= htmlspecialchars($orderData->order_id) ?></p>
+                        <p hidden><?= htmlspecialchars($orderData->id) ?></p>
+
+                        <!-- Número da encomenda -->
+                        <div
+                            class="badge bg-light-subtle border border-light-subtle text-light-emphasis rounded-pill position-absolute p-2 start-0 ms-3">
+                            <?= "#$orderData->order_number" ?>
+                        </div>
 
                         <!-- Icone da div -->
                         <div class="d-flex justify-content-center" style="margin: -50px auto 10px;">
@@ -35,23 +41,41 @@ FlashMessage::render();
                             <div class="row">
                                 <div class="col pe-0 mt-3">
 
-                                    <p class="mb-1" style="font-size: 1.1rem;"><strong>Encomenda</strong></p>
-                                    <p class="mb-3"><?= htmlspecialchars($orderData->order_quantity) . 'x ' . htmlspecialchars($orderData->order_title) ?></p>
+                                    <!-- Itens da encomenda -->
+                                    <h2 class="mb-1" style="font-size: 1.1rem;"><b>Encomenda</b></h2>
+                                    <?php foreach ($orderData->items as $item) { ?>
+                                        <p class="mb-1">
+                                            <?= htmlspecialchars($item->quantity) . 'x ' . htmlspecialchars($item->product_name) ?>
+                                        </p>
+                                    <?php } ?>
 
-                                    <div class="px-5">
-                                        <hr>
+                                    <hr class="px-5 w-75 mx-auto">
+
+                                    <!-- Dados da entrega -->
+                                    <h2 class="mb-1" style="font-size: 1.1rem;"><b>Entrega</b></h2>
+                                    <p class="mb-3">
+                                        <?= htmlspecialchars($orderData->completion_date) . ' às ' . htmlspecialchars($orderData->completion_time) ?>
+                                    </p>
+
+                                    <hr class="px-5 w-75 mx-auto">
+
+                                    <!-- Dados do cliente -->
+                                    <h2 class="mb-1" style="font-size: 1.1rem;"><b>Cliente</b></h2>
+                                    <p class="mb-3">
+                                        <?= htmlspecialchars($orderData->customer_name) ?>
+                                    </p>
+
+                                    <hr class="px-5 w-75 mx-auto">
+
+                                    <!-- Informaões auxiliares -->
+                                    <div class="mb-5">
+                                        <span
+                                            class="badge bg-info-subtle border border-info-subtle text-info-emphasis rounded-pill text-center">
+                                            Total R$ <?= $orderData->subtotal ?>
+                                        </span>
+                                        <?= $orderData->payment_status ?>
+                                        <?= $orderData->withdraw ?>
                                     </div>
-
-                                    <p class="mb-1" style="font-size: 1.1rem;"><strong>Entrega</strong></p>
-                                    <p class="mb-3"><?= htmlspecialchars($orderData->order_completion_date) ?><span class="text-danger-emphasis"><strong><?= htmlspecialchars($orderData->order_withdraw) ?></strong></span></p>
-
-                                    <div class="px-5">
-                                        <hr>
-                                    </div>
-
-                                    <p class="mb-1" style="font-size: 1.1rem;"><strong>Cliente</strong></p>
-                                    <p class="mb-3"><?= htmlspecialchars($orderData->order_client_name) ?></p>
-
                                 </div>
                             </div>
                         </div>
@@ -61,7 +85,7 @@ FlashMessage::render();
                             <div class="d-flex justify-content-center align-items-center">
                                 <button class="btn bg-body-secondary p-2 lh-1 rounded-5" type="button"
                                     title="Editar Encomenda"
-                                    onclick="window.location='<?= BASE_URL . '/order/edit/' . htmlspecialchars($orderData->order_id) ?>'"
+                                    onclick="window.location='<?= BASE_URL . '/encomenda/editar/' . htmlspecialchars($orderData->id) ?>'"
                                     style="width: 3.5rem; height: 3.5rem;">
                                     <i class="bi bi-three-dots-vertical" style="font-size: 1.5em;"></i>
                                 </button>
@@ -76,8 +100,8 @@ FlashMessage::render();
         <div class="position-fixed bottom-0 end-0 m-4">
             <button
                 class="btn bg-success-subtle text-success-emphasis rounded-5 p-3 shadow d-flex align-items-center justify-content-center"
-                type="button" title="Nova Encomenda" onclick="window.location='<?= BASE_URL . '/order/create' ?>'"
-                style="width: 4rem; height: 4rem;">
+                type="button" title="Nova Encomenda"
+                onclick="window.location='<?= BASE_URL . '/encomenda/cadastrar' ?>'" style="width: 4rem; height: 4rem;">
                 <i class="bi bi-plus" style="font-size: 2rem;"></i>
             </button>
         </div>
--- App/Views/order/show.php
+++ App/Views/order/show.php
@@ -8,29 +8,31 @@
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">Encomenda</th>
                             <th scope="col">Cliente</th>
                             <th scope="col">Entrega</th>
-                            <th scope="col">Preço</th>
-                            <th scope="col">Pagamento</th>
+                            <th scope="col">Subtotal</th>
+                            <th scope="col">Situação Pagamento</th>
+                            <th scope="col">Forma Pagamento</th>
                             <th scope="col">Status</th>
-                            <th scope="col">Editar</th>
+                            <th scope="col"></th>
+                            </th>
                         </tr>
                     </thead>
 
                     <tbody>
-                        <?php foreach ($ordersData as $orderData) { ?>
+                        <?php foreach ($viewData as $orderData) { ?>
                             <tr>
-                                <td><?= htmlspecialchars($orderData->order_id) ?></td>
-                                <td><?= htmlspecialchars($orderData->order_title) ?></td>
-                                <td><?= htmlspecialchars($orderData->order_client_name) ?></td>
-                                <td><?= htmlspecialchars($orderData->order_completion_date) ?></td>
-                                <td><?= 'R$' . htmlspecialchars($orderData->order_price) ?></td>
-                                <td><?= htmlspecialchars($orderData->order_payment_method) ?></td>
+                                <td><?= htmlspecialchars($orderData->order_number) ?></td>
+                                <td><?= htmlspecialchars($orderData->customer_name) ?></td>
+                                <td><?= htmlspecialchars($orderData->completion_date) ?></td>
+                                <td><?= 'R$ ' . htmlspecialchars($orderData->subtotal) ?></td>
+                                <td><?= $orderData->payment_status ?></td>
+                                <td><?= htmlspecialchars($orderData->payment_method) ?></td>
                                 <td><?= $orderData->order_status ?></td>
                                 <td>
                                     <button class="btn bg-body-secondary p-2 lh-1 rounded-5" type="button"
-                                        onclick="window.location='<?= BASE_URL . '/order/edit/' . htmlspecialchars($orderData->order_id) ?>'">
+                                        title="Editar Encomenda"
+                                        onclick="window.location='<?= BASE_URL . '/encomenda/editar/' . htmlspecialchars($orderData->id) ?>'">
                                         <i class="bi bi-three-dots-vertical" style="font-size: 1.3em;"></i>
                                     </button>
                                 </td>
@@ -45,8 +47,8 @@
         <div class="position-fixed bottom-0 end-0 m-4">
             <button
                 class="btn bg-success-subtle text-success-emphasis rounded-5 p-3 shadow d-flex align-items-center justify-content-center"
-                type="button" title="Nova Encomenda" onclick="window.location='<?= BASE_URL . '/order/create' ?>'"
-                style="width: 4rem; height: 4rem;">
+                type="button" title="Nova Encomenda"
+                onclick="window.location='<?= BASE_URL . '/encomenda/cadastrar' ?>'" style="width: 4rem; height: 4rem;">
                 <i class="bi bi-plus" style="font-size: 2rem;"></i>
             </button>
         </div>
