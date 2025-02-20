f8d94b1b54cd1c7e566ab7a46cef9891f98deb22
Gustavo Arantes <gustavoarantes@gmail.com>
2025-02-19 21:57:23 -0300

    feat: Adicionar classe para tratar login

---
+++ App/Helpers/login/Authenticator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Helpers\login;
+
+use App\Models\UserModel;
+
+/**
+ * Classe responável por gerenciar o login do usuário.
+ *
+ * Essa classe lida com o login, logout e verificação se o usuário está logado.
+ */
+abstract class Authenticator
+{
+    /**
+     * Autentica o usuário utilizando o e-mail e senha informados.
+     *
+     * @param string $email E-mail do usuário.
+     * @param string $password Senha do usuário.
+     * @return bool
+     */
+    public static function authenticate(string $email, string $password): bool
+    {
+        $userModel = new UserModel();
+
+        // Armazena os dados do usuário encontrado.
+        $userData = $userModel->getByEmail($email);
+
+        // Verifica se o usuário foi encontrado e compara a senha.
+        if ($userData && password_verify($password, $userData->password_hash)) {
+            $_SESSION['user_id'] = $userData->id;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Verifica se o usuário está autenticado.
+     *
+     * @return bool
+     */
+    public static function isAuthenticated(): bool
+    {
+        return isset($_SESSION['user_id']);
+    }
+
+    /**
+     * Faz o Logout do usuário.
+     *
+     * @return void
+     */
+    public static function logout(): void
+    {
+        session_unset();
+        session_destroy();
+
+        header('Location:' . BASE_URL);
+        exit;
+    }
+}
